4611a0ecc03b61ced591607fcad3641d6027c7f1
Bruna Borges <brunaborgesdev@gmail.com>
2025-10-01 19:18:24 -0300

    feat: applying best pratices for img bg and profile

--- src/app/components/AvatarArea.tsx
+++ src/app/components/AvatarArea.tsx
@@ -7,11 +7,11 @@ export const AvatarArea = () => {
       <Image
         alt='Avatar  de Bruna Borges Desenvolvedora Web'
         src='/profile.png'
-        width={350}
-        height={350}
+        width={250}
+        height={250}
         className='md:w-[250px] lg:w-[250px]' />
       <div className='py-4 text-3xl text-pink-600 font-bold text-center'>
-        <h1 className=''>Bruna Borges</h1>
+        <h1>Bruna Borges</h1>
         <h3 className='text-2xl leading-5 font-normal'>Software Developer</h3>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,18 @@ import { LinkCard } from "./components/LinkCard";
 
 export default function Home() {
   return (
-    <div className="bg-[url(/background.svg)] h-screen bg-center bg-cover flex flex-col justify-center items-center">
-      <main className="bg-transparent mx-auto w-96 flex flex-col items-center">
-        <AvatarArea />
-        <section className="flex flex-col p-5bg-transparent mx-auto rounded-2xl shadow-xl border border-indigo-900/70 p-5 gap-4">
-          <LinkCard href="https://www.linkedin.com/in/brunacborgesm/" title="Conecte-se ao meu LinkedIn" variant="linkedin" />
-          <LinkCard href="/" title="Meu site pessoal" variant="website" />
-          <LinkCard href="https://www.instagram.com/brunacarolinaa/" title="Siga-me no Instagram" variant="instagram" />
-          <LinkCard href="https://www.instagram.com/bmtechlab/" title="Conheça minha empresa @bmtechlab" variant="instagram" />
-        </section>
+    <main className="mx-auto w-full max-w-sm px-4 py-10">
+      <AvatarArea />
+      <section className="flex flex-col p-5bg-transparent mx-auto rounded-2xl shadow-xl border border-indigo-900/70 p-5 gap-4">
+        <LinkCard href="https://www.linkedin.com/in/brunacborgesm/" title="Conecte-se ao meu LinkedIn" variant="linkedin" />
+        <LinkCard href="/" title="Meu site pessoal" variant="website" />
+        <LinkCard href="https://www.instagram.com/brunacarolinaa/" title="Siga-me no Instagram" variant="instagram" />
+        <LinkCard href="https://www.instagram.com/bmtechlab/" title="Conheça minha empresa @bmtechlab" variant="instagram" />
+      </section>
 
-      </main>
       <footer className="flex text-slate-100 items-center justify-center pt-2">
         <span>Feito por <Link target="_blank" rel="noopener noreferrer" className="text-pink-600" href={"https://www.instagram.com/bmtechlab/"}>@BMTechLab</Link></span>
       </footer>
-    </div>
+    </main>
   );
 }
